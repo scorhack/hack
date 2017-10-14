bca78022c58fa0a7475de0f34a93f46edaac3d90
Andreas H. Klostergaard <aklost11@student.aau.dk>
2017-10-14 15:21:47 +0200

    testing

--- unblur.js
+++ unblur.js
@@ -1,4 +1,5 @@
 var imageEnding = "";
+var doc = null;
 
 function getImageEnding() {
 	var profileImg = document.getElementsByClassName("ProfilePictureLarge")[0];
@@ -16,7 +17,7 @@ function updateImages() {
 		return;
 	}
 
-	for (i of document.querySelectorAll("img[src]")) {
+	for (i of doc.querySelectorAll("img[src]")) {
 		i.src = i.src.slice(0, (i.src.indexOf("?") > -1 ? i.src.indexOf("?") : i.src.length + 1) - 5) + imageEnding;
 	}
 }
@@ -27,13 +28,16 @@ function prepareFrame() {
 	ifrm.style.width = "100%";
 	ifrm.style.height = "100%";
 	ifrm.style.position = "fixed";
+  ifrm.style.top = "0";
+  ifrm.style.left = "0";
   ifrm.style.zIndex = "100";
 	ifrm.onload = "updateImages()";
 	document.body.appendChild(ifrm);
+  doc = ifrm.contentDocument || ifrm.contentWindow.dokument;
 }
 
 
-//prepareFrame();
+prepareFrame();
 
 setTimeout(function(){
 	alert("Hvis du skulle have lyst til at kontakte mig, skal i være velkommen til at sende en snap til: kloster1992");
